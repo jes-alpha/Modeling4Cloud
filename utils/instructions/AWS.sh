a71793d95af90a9f0941e9f707ce1fd9d94b61a3
jessix <jes.alpha@gmail.com>
2019-02-06 21:30:40 +0100

    aws ip update7

--- utils/instructions/AWS.sh
+++ utils/instructions/AWS.sh
@@ -2,16 +2,16 @@
 ####Settings for hping direct tests
 #Provider, source-ssh-keyfile source-ip destination-ip source-zone destination-zone sequence-number
 pings=(
-"AWS ./keys/jBiancoNVirginia.pem 18.234.104.108 54.147.108.66 us-east-1c us-east-1a 1"
-"AWS ./keys/jBiancoNVirginia.pem 18.234.104.108 34.207.250.169 us-east-1c us-east-1c 2"
-"AWS ./keys/jBiancoNVirginia.pem 54.147.108.66 34.207.250.169 us-east-1a us-east-1c 3"
+"AWS ./keys/jBiancoNVirginia.pem 34.238.82.72 3.94.112.108 us-east-1c us-east-1a 1"
+"AWS ./keys/jBiancoNVirginia.pem 34.238.82.72 52.90.44.13 us-east-1c us-east-1c 2"
+"AWS ./keys/jBiancoNVirginia.pem 3.94.112.108 52.90.44.13 us-east-1a us-east-1c 3"
 	)
 
 
 ####Settings for iperf direct tests
 #Provider, source-ssh-keyfile source-ip destination-ssh-keyfile destination-ip source-zone destination-zone connection-port sequence-number hour-interval duration parallel
 iperfs=(
-	"AWS ./keys/jBiancoNVirginia.pem 18.234.104.108 ./keys/jBiancoIreland.pem 63.32.109.27 us-east-1c eu-west-1c 80 1 1 1 1"
+	"AWS ./keys/jBiancoNVirginia.pem 34.238.82.72 ./keys/jBiancoIreland.pem 63.32.109.27 us-east-1c eu-west-1c 80 1 1 1 1"
 		"AWS ./keys/jBiancoLondon.pem 3.8.2.35  ./keys/jBiancoIreland.pem 63.32.109.27 eu-west-2a eu-west-1c 80 2 1 1 1" 
 	)
 
@@ -19,7 +19,7 @@ iperfs=(
 ####Settings for qperf direct tests
 #Provider, source-ssh-keyfile source-ip destination-ssh-keyfile destination-ip source-zone destination-zone connection-port sequence-number
 qperfs=(
-	"AWS ./keys/jBiancoIreland.pem 63.32.109.27 ./keys/jBiancoNVirginia.pem 18.234.104.108 eu-west-1c us-east-1c 8080 1"
+	"AWS ./keys/jBiancoIreland.pem 63.32.109.27 ./keys/jBiancoNVirginia.pem 34.238.82.72 eu-west-1c us-east-1c 8080 1"
 	)
 
 
@@ -41,7 +41,7 @@ IPERF_BIDIRECTIONAL=1 #Test in both directions between regions(1) or not(0)
 
 #Information about vms to use for the cross region tests
 vms=(
-"./keys/jBiancoNVirginia.pem 18.234.104.108 us-east-1c"
+"./keys/jBiancoNVirginia.pem 34.238.82.72 us-east-1c"
 "./keys/jBiancoIreland.pem 63.32.109.27 eu-west-1c"
 "./keys/jBiancoLondon.pem 3.8.2.35 eu-west-2a"
 )
