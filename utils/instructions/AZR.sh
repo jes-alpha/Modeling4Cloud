bdbe0f34c04b366b40d1a1efad4bb35eb65c9e5a
jessix <jes.alpha@gmail.com>
2019-02-08 23:00:48 +0100

    ip updates

--- utils/instructions/AZR.sh
+++ utils/instructions/AZR.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Provider, source-ssh-keyfile source-ip destination-ip source-zone destination-zone sequence-number
 pings=(
-    "AZR ./keys/jBianco 13.79.31.97 52.187.243.229 north-eu aus 1"
+    "AZR ./keys/jBianco 40.113.70.169 104.214.219.149 north-eu west-eu 1"
     )
 
 #Provider, source-ssh-keyfile source-ip destination-ssh-keyfile destination-ip source-zone destination-zone connection-port sequence-number hour-interval duration parallel
-iperfs=("AZR ./keys/jBianco 13.79.31.97 ./keys/jBianco 52.187.243.229 north-eu aus 80 1 14 1 1")
+iperfs=("AZR ./keys/jBianco 40.113.70.169 ./keys/jBianco 104.214.219.149 north-eu west-eu 80 1 14 1 1")
 
 #Provider, source-ssh-keyfile source-ip destination-ssh-keyfile destination-ip source-zone destination-zone connection-port sequence-number
-qperfs=("AZR ./keys/jBianco 13.79.31.97 ./keys/jBianco 52.187.243.229 north-eu aus 8080 1")
+qperfs=("AZR ./keys/jBianco 40.113.70.169 ./keys/jBianco 104.214.219.149 north-eu west-eu 8080 1")
