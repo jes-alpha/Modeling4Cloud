90ad04edb85326cd17af624ede11c2f6188cfd06
jessix <jes.alpha@gmail.com>
2019-02-11 20:18:27 +0100

    ip update

--- utils/instructions/AWS.sh
+++ utils/instructions/AWS.sh
@@ -2,27 +2,23 @@
 ####Settings for hping direct tests
 #Provider, source-ssh-keyfile source-ip destination-ip source-zone destination-zone sequence-number
 pings=(
-		"AWS ./keys/aws-imol-virginia.pem 54.147.226.152 18.130.223.116 us-east-1 eu-west-2c 2" 
-		"AWS ./keys/aws-imol-tokyo.pem 18.179.61.138 18.130.223.116 ap-northeast-1 eu-west-2c 3" 
-		"AWS ./keys/aws-imol-virginia.pem 54.147.226.152 18.179.61.138 ap-northeast-1 us-east-1 4")
+		"AWS ./keys/jBiancoNVirginia.pem 18.205.114.73 18.232.71.179 us-east-1c us-east-1a 1" 
+		"AWS ./keys/jBiancoNVirginia.pem 184.73.29.174 18.232.71.179 us-east-1a us-east-1a 2" 
+		"AWS ./keys/jBiancoNVirginia.pem 18.205.114.73 184.73.29.174 us-east-1c us-east-1a 3")
 
 
 
 ####Settings for iperf direct tests
 #Provider, source-ssh-keyfile source-ip destination-ssh-keyfile destination-ip source-zone destination-zone connection-port sequence-number hour-interval duration parallel
-iperfs=("AWS ./keys/aws-imol-london.pem 18.130.223.116 ./keys/aws-imol-paris.pem 35.180.92.154 eu-west-2c eu-west-3c 80 1 14 1 1" 
-	"AWS ./keys/aws-imol-virginia.pem 54.147.226.152 ./keys/aws-imol-london.pem 18.130.223.116 us-east-1 eu-west-2c 80 2 14 1 1" 
-	"AWS ./keys/aws-imol-tokyo.pem 18.179.61.138 ./keys/aws-imol-london.pem 18.130.223.116 ap-northeast-1 eu-west-2c 80 3 14 1 1" 
-	"AWS ./keys/aws-imol-virginia.pem 54.147.226.152 ./keys/aws-imol-tokyo.pem 18.179.61.138 ap-northeast-1 us-east-1 80 4 14 1 1")
-
-
+iperfs=("AWS ./keys/jBiancoNVirginia.pem 18.205.114.73 ./keys/jBiancoNVirginia.pem 184.73.29.174 us-east-1c us-east-1a 80 1 14 1 1" 
+	"AWS ./keys/jBiancoNVirginia.pem 18.205.114.73 ./keys/jBiancoIreland.pem 34.244.169.175 us-east-1c eu-west-1c 80 2 14 1 1" 
+	"AWS ./keys/jBiancoLondon.pem 18.130.96.67 ./keys/jBiancoIreland.pem 34.244.169.175 eu-west-2a eu-west-1c 80 3 14 1 1" 
+	"AWS ./keys/jBiancoLondon.pem 18.130.96.67 ./keys/jBiancoNVirginia.pem 18.205.114.73 eu-west-2a us-east-1c 80 4 14 1 1")
 
 
 ####Settings for qperf direct tests
 #Provider, source-ssh-keyfile source-ip destination-ssh-keyfile destination-ip source-zone destination-zone connection-port sequence-number
-qperfs=("AWS ./keys/aws-imol-paris.pem 35.180.92.154 ./keys/aws-imol-london.pem 18.130.223.116 eu-west-3c eu-west-2c 8080 1")
-
-
+#qperfs=("AWS ./keys/aws-imol-paris.pem 35.180.92.154 ./keys/aws-imol-london.pem 18.130.223.116 eu-west-3c eu-west-2c 8080 1")
 
 
 ####Settings for crossregion and sameregion tests
@@ -43,19 +39,7 @@ IPERF_BIDIRECTIONAL=0 #Test in both directions between regions(1) or not(0)
 
 #Information about vms to use for the cross region tests
 vms=(
-"./keys/aws-imol-virginia.pem 107.21.185.163 us-east-1"
-"./keys/aws-imol-ohio.pem 18.222.248.68 us-east-2"
-"./keys/aws-imol-california.pem 54.183.120.193 us-west-1"
-"./keys/aws-imol-oregon.pem 54.200.106.63 us-west-2"
-"./keys/aws-imol-mumbai.pem 35.154.221.120 ap-south-1"
-"./keys/aws-imol-seoul.pem 52.79.107.222 ap-northeast-2"
-"./keys/aws-imol-singapore.pem 54.169.139.66 ap-southeast-1"
-"./keys/aws-imol-sydney.pem 54.206.99.141 ap-southeast-2"
-"./keys/aws-imol-tokyo.pem 18.179.61.138 ap-northeast-1"
-"./keys/aws-imol-canada.pem 35.183.41.6 ca-central-1"
-"./keys/aws-imol-frankfurt.pem 35.159.33.175 eu-central-1"
-"./keys/aws-imol-ireland.pem 34.249.20.202 eu-west-1"
-"./keys/aws-imol-london.pem 18.130.223.116 eu-west-2"
-"./keys/aws-imol-paris.pem 35.180.92.154 eu-west-3"
-"./keys/aws-imol-saopaulo.pem 18.231.177.17 sa-east-1"
+"./keys/jBiancoNVirginia.pem 18.205.114.73 us-east-1c"
+"./keys/jBiancoIreand.pem 18.205.114.73 us-east-1c"
+"./keys/jBiancoLondon.pem 18.205.114.73 us-east-1c"
 )
--- utils/instructions/deploy.sh
+++ utils/instructions/deploy.sh
@@ -2,13 +2,13 @@
 BACKENDADDRPINGS=http://54.234.131.88:3100/api/upload
 BACKENDADDRBANDWITDHS=http://54.234.131.88:3100/api/uploadBandwidths
 
-#./hping/setupHping.sh ./AWS.sh $BACKENDADDRPINGS
-./hping/setupHping.sh ./AZR.sh $BACKENDADDRPINGS
+./hping/setupHping.sh ./AWS.sh $BACKENDADDRPINGS
+#./hping/setupHping.sh ./AZR.sh $BACKENDADDRPINGS
 #./hping/setupHping.sh ./GCP.sh $BACKENDADDRPINGS
 #./hping/setupHping.sh ./IBM.sh $BACKENDADDRPINGS
 
-#./iperf/setupIperf.sh ./AWS.sh $BACKENDADDRBANDWITDHS
-./iperf/setupIperf.sh ./AZR.sh $BACKENDADDRBANDWITDHS
+./iperf/setupIperf.sh ./AWS.sh $BACKENDADDRBANDWITDHS
+#./iperf/setupIperf.sh ./AZR.sh $BACKENDADDRBANDWITDHS
 #./iperf/setupIperf.sh ./GCP.sh $BACKENDADDRBANDWITDHS
 #./iperf/setupIperf.sh ./IBM.sh $BACKENDADDRBANDWITDHS
 
@@ -17,5 +17,5 @@ BACKENDADDRBANDWITDHS=http://54.234.131.88:3100/api/uploadBandwidths
 #./qperf/setupQperf.sh ./GCP.sh $BACKENDADDRBANDWITDHS
 #./qperf/setupQperf.sh ./IBM.sh $BACKENDADDRBANDWITDHS
 
-#./hping/setupCrossHping.sh ./AWS.sh $BACKENDADDRPINGS
-#./iperf/setupCrossIperf.sh ./AWS.sh $BACKENDADDRBANDWITDHS
+./hping/setupCrossHping.sh ./AWS.sh $BACKENDADDRPINGS
+./iperf/setupCrossIperf.sh ./AWS.sh $BACKENDADDRBANDWITDHS
