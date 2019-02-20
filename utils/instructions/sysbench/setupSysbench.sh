737e12fc3d38a6642d11e0e1ea11755ee56078ff
jessix <jes.alpha@gmail.com>
2019-02-20 23:54:55 +0100

    wip

--- utils/instructions/sysbench/setupSysbench.sh
+++ utils/instructions/sysbench/setupSysbench.sh
@@ -14,13 +14,14 @@ do
     ssh -o StrictHostKeyChecking=no -i $KEY ubuntu@$HOST bash -c "'
 	mkdir -p ~/Modeling4Cloud/utils/
 	sudo apt-get update -qq
-    sudo apt-get install sysbench -qq'"
+    sudo apt-get install sysbench -qq
+    sudo apt-get install lscpu -qq'"
     
     scp -r -i $KEY ./sysbench/enableSysbench.sh ubuntu@$HOST:~
     scp -r -i $KEY ./sysbench/registerSysbenchCSV.sh ubuntu@$HOST:~/Modeling4Cloud/utils/
     scp -r -i $KEY ./sysbench/setupSysbench.sh ubuntu@$HOST:~/Modeling4Cloud/utils/
-    ssh -i $KEY ubuntu@$HOST bash -c "
+    ssh -i $KEY ubuntu@$HOST bash -c "'
     chmod +x ~/enableSysbench.sh
-    './enableSysbench.sh $PROVIDER $HOST $BACKENDADDR $INTERVAL '"
+    ./enableSysbench.sh $PROVIDER $HOST $BACKENDADDR $INTERVAL '"
     printf "_____ COMPLETE _____ \n\n\n\n"
 done
