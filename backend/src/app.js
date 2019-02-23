33cbeb31707e36db294934894a61b58b465cb556
jessix <jes.alpha@gmail.com>
2019-02-23 18:19:21 +0100

    debugging

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,22 +43,22 @@ router.get('/', function (req, res) {
     res.json({ message: 'API Initialized!' });
 });
 
-router.route('/uploadBenchmarks').post(uploadBM.single('data'), function (req, res) {
-    fs.rename(UPLOADBM_PATH + req.file.filename, UPLOADBM_PATH + req.file.originalname, function (err) {
-        if (err) return res.status(500).send("Problem in POST\n");
-        res.status(200).send("File registered\n");
-        console.log("File uploaded(Benchmark test):" + req.file.originalname);
-        const { exec } = require('child_process');
-        exec('tail -n +2 ' + UPLOADBM_PATH + req.file.originalname + ' |  sed \'/,,/d\' | sed \'/,SA,/d\'  | mongoimport --uri ' + db.url + ' -c benchmarks --type csv"', (err, stdout, stderr) => {
-        //exec('tail -n +2 ' + UPLOADBM_PATH + req.file.originalname + ' |  sed \'/,,/d\' | sed \'/,SA,/d\'  | mongoimport --uri ' + db.url + ' -c benchmarks --type csv --columnsHaveTypes --fields "provider.string\(\),ip.string\(\),timestamp.date\(2006-01-02T15:04:05-00:00\),threads.int32\(\),totalTime.int32\(\),totalEvents.int32\(\),cpus.int32\(\)"', (err, stdout, stderr) => {
-            if (err) {
-                // TODO
-                console.log(err)
-                return;
-            }
-        });
-    });
-});
+// router.route('/uploadBenchmarks').post(uploadBM.single('data'), function (req, res) {
+//     fs.rename(UPLOADBM_PATH + req.file.filename, UPLOADBM_PATH + req.file.originalname, function (err) {
+//         if (err) return res.status(500).send("Problem in POST\n");
+//         res.status(200).send("File registered\n");
+//         console.log("File uploaded(Benchmark test):" + req.file.originalname);
+//         const { exec } = require('child_process');
+//         exec('tail -n +2 ' + UPLOADBM_PATH + req.file.originalname + ' |  sed \'/,,/d\' | sed \'/,SA,/d\'  | mongoimport --uri ' + db.url + ' -c benchmarks --type csv"', (err, stdout, stderr) => {
+//         //exec('tail -n +2 ' + UPLOADBM_PATH + req.file.originalname + ' |  sed \'/,,/d\' | sed \'/,SA,/d\'  | mongoimport --uri ' + db.url + ' -c benchmarks --type csv --columnsHaveTypes --fields "provider.string\(\),ip.string\(\),timestamp.date\(2006-01-02T15:04:05-00:00\),threads.int32\(\),totalTime.int32\(\),totalEvents.int32\(\),cpus.int32\(\)"', (err, stdout, stderr) => {
+//             if (err) {
+//                 // TODO
+//                 console.log(err)
+//                 return;
+//             }
+//         });
+//     });
+// });
 
 router.route('/upload').post(upload.single('data'), function (req, res) {
     fs.rename(UPLOAD_PATH + req.file.filename, UPLOAD_PATH + req.file.originalname, function (err) {
