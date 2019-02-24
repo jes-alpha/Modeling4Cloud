1cefe185a8623b1de047bcd9f9b1c9ec46e11c7a
jessix <jes.alpha@gmail.com>
2019-02-24 01:08:37 +0100

    checkpoint

--- frontend/src/MyCard.js
+++ frontend/src/MyCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Tabs, Tab, FontIcon, DatePicker, Toggle, GridList, GridTile, SelectField, MenuItem, RaisedButton, TextField} from 'material-ui/';
+import { Card, Tabs, Tab, FontIcon, DatePicker, Toggle, GridList, GridTile, SelectField, MenuItem, RaisedButton, TextField } from 'material-ui/';
 import MyGraph from './MyGraph';
 import moment from "moment/moment";
 
@@ -48,7 +48,7 @@ const horizontalBarHelper = {
             borderWidth: 1,
             hoverBackgroundColor: 'rgba(255,99,132,0.4)',
             hoverBorderColor: 'rgba(255,99,132,1)',
-            backgroundColor:[
+            backgroundColor: [
                 'rgba(255, 99, 132, 0.6)',
                 'rgba(54, 162, 235, 0.6)',
                 'rgba(255, 206, 86, 0.6)',
@@ -216,7 +216,7 @@ const lineHelper = {
 }
 */
 export default class MyCard extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
@@ -235,60 +235,65 @@ export default class MyCard extends React.Component {
         }
     }
 
+    /* not used
     handleYearChange = (event, value) => {
         this.setState({year: parseInt(value)}, this.check);
         console.log("year "+ value);
-
     }
+    */
 
     handleProviderChange = (event, value) => {
-        this.setState({provider: value}, this.check);
-        console.log("provider "+ value);
-
+        this.setState({ provider: value }, this.check);
+        console.log("provider " + value);
     }
 
     handleZoneChange = (event, value) => {
-        this.setState({zone: value}, this.check);
-        console.log("zone"+ value);
-
+        this.setState({ zone: value }, this.check);
+        console.log("zone" + value);
     }
 
     handleQueryNumberChange = (event, index, value) => {
-        this.setState({queryNumber: value, buttonDisabled: true});
-        console.log("query number "+ value);
-
+        this.setState({ queryNumber: value, buttonDisabled: true });
+        console.log("query number " + value);
     }
 
     handleDataTypeChange = (event, index, value) => {
-        this.setState({dataType: value, buttonDisabled: true});
-        console.log("data type "+ value);
+        this.setState({ dataType: value, buttonDisabled: true }, this.check);
+        console.log("data type " + value);
     }
 
-    check(){
+    check() {
         console.log("check");
         console.log(this.state.dataType + "-" + this.state.queryNumber + "-" + this.state.start + "-" + this.state.end + "-" + this.state.crossRegion)
-        if(this.state.dataType && this.state.queryNumber && this.state.start && this.state.end){
-            switch(this.state.queryNumber){
+        if (this.state.dataType && this.state.queryNumber && this.state.start && this.state.end) {
+            switch (this.state.dataType){
+                case 'benchmarks': console.log("benchmarks case in check");
+                case 'pings':console.log("pings case in check");
+                case 'bandwiidths':console.log("bandwidths case in check");
+            
+            switch (this.state.queryNumber) {
                 case 1:
-                    this.setState({buttonDisabled: false})
+                    this.setState({ buttonDisabled: false })
                     break;
                 case 2:
                     if (this.state.provider) {
-                        this.setState({buttonDisabled: false})
+                        this.setState({ buttonDisabled: false })
                     }
                     break;
                 case 3:
                     if (this.state.provider && this.state.zone) {
-                        this.setState({buttonDisabled: false})
+                        this.setState({ buttonDisabled: false })
                     }
                     break;
                 default:
-                    this.setState({buttonDisabled: true})
+                    this.setState({ buttonDisabled: true })
                     break;
             }
+            default:
         }
-        else{
-            this.setState({buttonDisabled: true})
+        }
+        else {
+            this.setState({ buttonDisabled: true })
         }
     }
 
@@ -304,26 +309,27 @@ export default class MyCard extends React.Component {
         }, this.check);
     };
 
-    handleCrossRegionChange = (event, isInputChecked) =>{
-        this.setState({crossRegion: isInputChecked});
-        console.log("crss region switch "+isInputChecked);
+    handleCrossRegionChange = (event, isInputChecked) => {
+        this.setState({ crossRegion: isInputChecked });
+        console.log("crss region switch " + isInputChecked);
     }
 
 
-    handleRadioButton = (event, value) =>{
-        this.setState({graphType: value});
+    handleRadioButton = (event, value) => {
+        this.setState({ graphType: value });
     }
 
-    handleClick = (event) =>{
+    handleClick = (event) => {
+        console.log("handle click ");
         this.callApi()
             .then(res => {
 
-                res.sort(function(a, b){
+                res.sort(function (a, b) {
                     var keyA = a.avg,
                         keyB = b.avg;
                     // Compare the 2 values
-                    if(keyA < keyB) return -1;
-                    if(keyA > keyB) return 1;
+                    if (keyA < keyB) return -1;
+                    if (keyA > keyB) return 1;
                     return 0;
                 });
 
@@ -336,12 +342,10 @@ export default class MyCard extends React.Component {
                 datasetsModified[0].data.length = 0;
                 labelsModified.length = 0;
 
-
-
-                for(let resource of res){
-                    console.log("resource of res "+resource);
-                    console.log("state.querynumber: "+ this.state.queryNumber);
-                    switch(this.state.queryNumber){
+                for (let resource of res) {
+                    console.log("resource of res " + resource);
+                    console.log("state.querynumber: " + this.state.queryNumber);
+                    switch (this.state.queryNumber) {
                         case 1:
                             datasetsModified[0].label = "Comparison between providers based on average of all " + this.state.dataType;
                             labelsModified.push(resource.provider)
@@ -350,33 +354,35 @@ export default class MyCard extends React.Component {
                         case 2:
                         case 3:
                             datasetsModified[0].label = "Comparison between zones based on average of all " + this.state.dataType;
-                            labelsModified.push(resource.provider + ":" +resource.from_zone + "->" + resource.to_zone)
+                            labelsModified.push(resource.provider + ":" + resource.from_zone + "->" + resource.to_zone)
                             datasetsModified[0].data.push(resource.avg)
                             break;
+                        default:
+
                     }
                 }
 
-                this.setState({graphData: graphDataModified, graphType: graphType, buttonClicked: true, redraw: true})
+                this.setState({ graphData: graphDataModified, graphType: graphType, buttonClicked: true, redraw: true })
 
             })
-            .then(() =>{
+            .then(() => {
                 this.resetForm()
             })
             .catch(err => console.log(err))
-    }
+    } // TODO
 
-    async callApi(){
+    async callApi() {
         var query;
-        console.log("call api "+this.state.queryNumber);
-        switch(this.state.queryNumber){
+        console.log("call api: " + this.state.queryNumber);
+        switch (this.state.queryNumber) {
             case 1:
-                query = backendAddress+this.state.dataType+'/query/avgOfSelectedDate?crossRegion='+((this.state.crossRegion === true) ? 1 : 0)+'&start='+moment(this.state.start).format('YYYY-MM-DD')+'&end='+moment(this.state.end).format('YYYY-MM-DD')
+                query = backendAddress + this.state.dataType + '/query/avgOfSelectedDate?crossRegion=' + ((this.state.crossRegion === true) ? 1 : 0) + '&start=' + moment(this.state.start).format('YYYY-MM-DD') + '&end=' + moment(this.state.end).format('YYYY-MM-DD')
                 break;
             case 2:
-                query = backendAddress+this.state.dataType+'/query/avgOfProviderOfSelectedDate?provider='+this.state.provider+'&start='+moment(this.state.start).format('YYYY-MM-DD')+'&end='+moment(this.state.end).format('YYYY-MM-DD')
+                query = backendAddress + this.state.dataType + '/query/avgOfProviderOfSelectedDate?provider=' + this.state.provider + '&start=' + moment(this.state.start).format('YYYY-MM-DD') + '&end=' + moment(this.state.end).format('YYYY-MM-DD')
                 break;
             case 3:
-                query = backendAddress+this.state.dataType+'/query/avgOfZoneOfSelectedDate?provider='+this.state.provider+'&zone='+this.state.zone+'&start='+moment(this.state.start).format('YYYY-MM-DD')+'&end='+moment(this.state.end).format('YYYY-MM-DD')
+                query = backendAddress + this.state.dataType + '/query/avgOfZoneOfSelectedDate?provider=' + this.state.provider + '&zone=' + this.state.zone + '&start=' + moment(this.state.start).format('YYYY-MM-DD') + '&end=' + moment(this.state.end).format('YYYY-MM-DD')
                 break;
             default:
 
@@ -390,79 +396,98 @@ export default class MyCard extends React.Component {
         return body;
     }
 
-    resetForm = () =>{
+    resetForm = () => {
         //this.setState({start: null, end: null, buttonDisabled: true})
     }
 
-    renderFirst(){
-        switch(this.state.queryNumber){
+    renderZero() {
+         console.log("render zero " + this.state.dataType);
+        switch (this.state.dataType) {
             case 1:
-                return(
-                    <DatePicker hintText="Select start date" value={this.state.start} onChange={this.handleStartChange}/>
+            case 2:
+            case 3:
+            case 4:
+            default: console.log("render zero " + this.state.dataType);
+        }
+
+    }
+
+    renderFirst() {
+        switch (this.state.queryNumber) {
+            case 1:
+                return (
+                    <DatePicker hintText="Select start date" value={this.state.start} onChange={this.handleStartChange} />
                     //<TextField hintText="Es: 2018" floatingLabelText="Insert year"/>
                     //<DatePicker hintText="Select start year" value={this.state.start} onChange={this.handleStartChange}/>
                 )
             case 2:
             case 3:
-                return(
-                    <TextField hintText="Select provider (es: 'AWS')" onChange={this.handleProviderChange}/>
+                return (
+                    <TextField hintText="Select provider (es: 'AWS')" onChange={this.handleProviderChange} />
                 )
             case 4:
-                return(
-                    <DatePicker hintText="Select start day" value={this.state.start} onChange={this.handleStartChange}/>
+                return (
+                    <DatePicker hintText="Select start day" value={this.state.start} onChange={this.handleStartChange} />
                 )
+            default:
         }
 
     }
 
-    renderSecond(){
-        switch(this.state.queryNumber){
+    renderSecond() {
+        switch (this.state.queryNumber) {
             case 1:
-                return(
-                    <DatePicker hintText="Select end date" value={this.state.end} onChange={this.handleEndChange}/>
+                return (
+                    <DatePicker hintText="Select end date" value={this.state.end} onChange={this.handleEndChange} />
                     //<TextField hintText="Es: 'AWS'" floatingLabelText="Insert provider"/>
                     //<DatePicker hintText="Select end year" value={this.state.end} onChange={this.handleEndChange}/>
                 )
             case 2:
             case 3:
-                return(
-                    <DatePicker hintText="Select start date" value={this.state.start} onChange={this.handleStartChange}/>
+                return (
+                    <DatePicker hintText="Select start date" value={this.state.start} onChange={this.handleStartChange} />
                 )
             case 4:
-                return(
-                    <DatePicker hintText="Select end day" value={this.state.end} onChange={this.handleEndChange}/>
+                return (
+                    <DatePicker hintText="Select end day" value={this.state.end} onChange={this.handleEndChange} />
                 )
+            default:
+
         }
     }
 
-    renderThird(){
-        switch(this.state.queryNumber){
+    renderThird() {
+        switch (this.state.queryNumber) {
             case 1:
-                return(<Toggle label="Cross Region" style={{padding: '10px', width: '90%'}} defaultToggled={true} onToggle={this.handleCrossRegionChange}/>)
+                return (<Toggle label="Cross Region" style={{ padding: '10px', width: '90%' }} defaultToggled={true} onToggle={this.handleCrossRegionChange} />)
             case 2:
             case 3:
-                return(
-                    <DatePicker hintText="Select end date" value={this.state.end} onChange={this.handleEndChange}/>
+                return (
+                    <DatePicker hintText="Select end date" value={this.state.end} onChange={this.handleEndChange} />
                 )
             case 4:
-                return(
-                    <DatePicker hintText="Select end day" value={this.state.end} onChange={this.handleEndChange}/>
+                return (
+                    <DatePicker hintText="Select end day" value={this.state.end} onChange={this.handleEndChange} />
                 )
+            default:
+
         }
     }
 
-    renderFourth(){
-        switch(this.state.queryNumber){
+    renderFourth() {
+        switch (this.state.queryNumber) {
             case 1:
-                return(null)
+                return (null)
             case 2:
-                return(null)
+                return (null)
             case 3:
-                return(
-                    <TextField hintText="Select zone (es: 'eu-west-1')" onChange={this.handleZoneChange}/>
+                return (
+                    <TextField hintText="Select zone (es: 'eu-west-1')" onChange={this.handleZoneChange} />
                 )
             case 4:
-                return(null)
+                return (null)
+            default:
+
         }
     }
 
@@ -498,11 +523,12 @@ export default class MyCard extends React.Component {
                                         <MenuItem value={2} primaryText="Single Provider" />
                                         <MenuItem value={3} primaryText="Single Zone" />
                                     </SelectField>
+                                    {this.renderZero()}
                                     {this.renderFirst()}
                                     {this.renderSecond()}
                                     {this.renderThird()}
                                     {this.renderFourth()}
-                                {/*<GridTile>
+                                    {/*<GridTile>
                                     <RadioButtonGroup name="graphType" defaultSelected="Pie" style={{display: 'inline'}} onChange={this.handleRadioButton}>
                                         <RadioButton value="Pie" label="Pie"/>
                                         <RadioButton value="Doughnut" label="Doughnut"/>
@@ -510,14 +536,14 @@ export default class MyCard extends React.Component {
                                         <RadioButton value="Line" label="Line"/>
                                     </RadioButtonGroup>
                                 </GridTile>*/}
-                                    <RaisedButton label="Send" secondary={true} disabled={this.state.buttonDisabled} onClick={this.handleClick} style={{marginLeft: '27%'}}/>
+                                    <RaisedButton label="Send" secondary={true} disabled={this.state.buttonDisabled} onClick={this.handleClick} style={{ marginLeft: '27%' }} />
                                 </GridTile>
                             </GridList>
                         </div>
                     </Tab>
                     <Tab icon={<FontIcon className="material-icons" >pie_chart</FontIcon>} label="Graph">
                         <div>
-                            { this.state.buttonClicked ? <MyGraph data={this.state.graphData} graphType={this.state.graphType}/> : null }
+                            {this.state.buttonClicked ? <MyGraph data={this.state.graphData} graphType={this.state.graphType} /> : null}
                         </div>
                     </Tab>
                 </Tabs>
